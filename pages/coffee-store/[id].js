d40270a4a87e09c938fbefbba54378698cf118f7
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-10-31 00:00:27 +0330

    deploy FourSquare API on application to indicate real data

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,44 +1,51 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
-import cls from "classnames";
-import coffeeStoresData from "../../data/coffee-stores.json";
+import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
+
+import cls from "classnames";
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
+import coffeeStoresData from "../../data/coffee-stores.json";
+
 import styles from "../../styles/coffee-store.module.css";
 
-export function getStaticProps(staticProps) {
+export async function getStaticProps(staticProps) {
   const params = staticProps.params;
+  console.log("params", params);
+  const coffeeStores = await fetchCoffeeStores();
   return {
     props: {
-      coffeeStore: coffeeStoresData.find((coffeeStore) => {
-        return coffeeStore.id.toString() === params.id;
+      coffeeStore: coffeeStores.find((coffeeStore) => {
+        return coffeeStore.fsq_id.toString() === params.id; //dynamic id
       }),
     },
   };
 }
 
-export function getStaticPaths() {
-  const paths = coffeeStoresData.map((coffeeStore) => {
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = coffeeStores.map((coffeeStore) => {
     return {
       params: {
-        id: coffeeStore.id.toString(),
+        id: coffeeStore.fsq_id.toString(),
       },
     };
   });
-  return { paths, fallback: true };
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
 const CoffeeStore = (props) => {
   const router = useRouter();
-
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
-  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
-  const hamdleUpvoteButton = () => {
-    console.log("handle upvote");
-  };
+  const { location, name, imgUrl } = props.coffeeStore;
+
+  const handleUpvoteButton = () => {};
 
   return (
     <div className={styles.layout}>
@@ -56,28 +63,32 @@ const CoffeeStore = (props) => {
             <h1 className={styles.name}>{name}</h1>
           </div>
           <Image
-            src={imgUrl}
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
             width={600}
             height={360}
             className={styles.storeImg}
             alt={name}
-          ></Image>
+          />
         </div>
+
         <div className={cls("glass", styles.col2)}>
           <div className={styles.iconWrapper}>
-            <Image src="/static/icons/places.svg" width={24} height={24} />
-            <p className={styles.text}>{address}</p>
+            <Image src="/static/icons/places.svg" width="24" height="24" />
+            <p className={styles.text}>{location.address}</p>
           </div>
           <div className={styles.iconWrapper}>
-            <Image src="/static/icons/nearMe.svg" width={24} height={24} />
-            <p className={styles.text}>{neighbourhood}</p>
+            <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+            <p className={styles.text}>{location.formatted_address}</p>
           </div>
           <div className={styles.iconWrapper}>
-            <Image src="/static/icons/star.svg" width={24} height={24} />
+            <Image src="/static/icons/star.svg" width="24" height="24" />
             <p className={styles.text}>1</p>
           </div>
 
-          <button className={styles.upvoteButton} onClick={hamdleUpvoteButton}>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
             Up vote!
           </button>
         </div>
